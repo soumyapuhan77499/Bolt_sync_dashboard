7796140c0d5505471c0a6623e0a955b8b71d5d39
soumyadeveloper1612 <soumyadeveloper1612@gmail.com>
2026-04-17 18:52:13 +0530

    dd

--- app/Models/AdminUser.php
+++ app/Models/AdminUser.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class AdminUser extends Model
 {
-    //
+    protected $table = 'admin_users';
+
+    protected $fillable = [
+        'name',
+        'user_id',
+        'email',
+        'password',
+        'status',
+    ];
+
+    protected $hidden = [
+        'password',
+    ];
 }
